9755dde013be6f69dd5bb818ba5509bdb81ad08f
Urjashee Shaw <urjashee09@gmail.com>
2024-03-07 17:56:00 +0530

    removed device token from web login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -84,8 +84,9 @@ class LoginController extends Controller
 
             if (Hash::check($request->input("password"), $user->password)) {
                 list($token, $refreshToken) = UserFunctions::generateToken($user);
-
-                $this->deviceToken($user, $request, $token);
+                if ($request->has("device_token")) {
+                    $this->deviceToken($user, $request, $token);
+                }
 
                 return ResponseFormatter::successResponse("Login successful.",
                     array("token" => (string)$token, "refresh_token" => (string)$refreshToken))
