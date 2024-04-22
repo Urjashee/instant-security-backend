d930107f25d381c92b12fe1c7b880b6498f1cece
Urjashee Shaw <urjashee09@gmail.com>
2024-04-22 21:48:44 +0530

    reviews for user role

--- app/Common/FunctionHelpers/JobFunctions.php
+++ app/Common/FunctionHelpers/JobFunctions.php
@@ -319,7 +319,7 @@ class JobFunctions
 //                "job_customer_name" => $job->users->first_name . " " . $job->users->last_name,
 //            ];
 //        }
-        if ($role == 2 || $role == 1) {
+        if ($role == 2 || $role == 1 || $role == 3) {
             $job_review = JobReview::where("job_id", $job->id)->first();
             if ($job_review) {
                 $content_data += [
